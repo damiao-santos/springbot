17a26bb41239469d211c6f0d06d110aec3cc925f
damiao tenorio <damiaotenorio@gmail.com>
2024-05-04 01:35:39 -0300

    fixed path to push on workflow

--- src/main/java/com/example/Springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/example/Springbootapp/SpringBootAppApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SpringBootAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAppApplication.class, args);
-	}
+   @RequestMapping("/")
+   public String home() {
+       return "Hello Kubernetes!";
+   }
+
+   public static void main(String[] args) {
+       SpringApplication.run(SpringBootAppApplication.class, args);
+   }
 
 }
